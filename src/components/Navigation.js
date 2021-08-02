9e79d3e276a76aa95720550af165cdf26e04778e
Liviu Bobocu <liviualexandrubobocu@gmail.com>
2021-08-03 00:47:39 +0300

    Optimize build

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,17 @@
+  import React, {Suspense} from 'react';
 import {Switch, Route} from "react-router-dom";
-import Transactions from './transactions/Transactions';
-import Home from '../pages/Home';
-import Departments from "../pages/Departments";
+// import Transactions from './transactions/Transactions';
+// import Home from '../pages/Home';
+// import Departments from "../pages/Departments";
+
+const Home = React.lazy(() =>import(/* webpackPrefetch: true, webpackChunkName:"Home" */"../pages/Home"));
+const Transactions = React.lazy(() =>import(/* webpackPrefetch: true, webpackChunkName:"Transactions" */"./transactions/Transactions"));
+const Departments = React.lazy(() =>import(/* webpackPrefetch: true, webpackChunkName:"Departments" */"../pages/Departments"));
+
 
 const Navigation = () => {
     return (
-        <>
+        <Suspense fallback={(<div>Loading...</div>)}>
             <Switch>
                 <Route exact path="/">
                     <Home/>
@@ -17,7 +23,7 @@ const Navigation = () => {
                     <Departments/>
                 </Route>
             </Switch>
-        </>
+        </Suspense>
     )
 }
 
